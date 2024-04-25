044650eeb799d0e20a80f667a80afd03600b3572
Florian Maurer <maurer@fh-aachen.de>
2024-04-25 09:26:17 +0200

    add amiris_run file adjust storage

--- examples/amiris_run.py
+++ examples/amiris_run.py
@@ -1,24 +1,28 @@
-# SPDX-FileCopyrightText: ASSUME Developers
+# SPDX-FileCopyrightText: Florian Maurer
 #
-# SPDX-License-Identifier: AGPL-3.0-or-later
+# SPDX-License-Identifier: Apache-2.0
 
+import logging
 import time
+from importlib import reload
 from pathlib import Path
 
 import pandas as pd
-from amirispy.scripts import amiris_cli
 from sqlalchemy import create_engine, text
 
 db_uri = "postgresql://assume:assume@localhost:5432/assume"
 engine = create_engine(db_uri)
 
 scenario = "Germany2019"  # "Simple", "Germany2019", "Austria2019"
-run_amiris = True
+run_amiris = False
 run_assume = True
 
 scenario_path = f"../amiris-examples/{scenario}"
 
 if run_amiris:
+    # pip install amirispy
+    from amirispy.scripts import amiris_cli
+
     amiris_path = Path("./amiris-core_*-jar-with-dependencies.jar")
     amiris_paths = list(Path(amiris_path.parent).glob(amiris_path.name))
 
@@ -32,7 +36,7 @@ if run_amiris:
     start = time.time()
     executable = amiris_paths[0].name
     amiris_cli(
-        f"run --jar ./{executable} -s {scenario_path}/scenario.yaml --output {scenario}".split(
+        f"run --jar ./{executable} -s {scenario_path}/scenario.yaml --output scenario/{scenario}".split(
             " "
         )
     )
@@ -44,33 +48,48 @@ if run_amiris:
     with engine.begin() as conn:
         conn.execute(query)
 
-    for output_file in Path(scenario).glob("*.csv"):
+    for output_file in (Path("scenario") / scenario).glob("*.csv"):
+        with open(output_file, "r") as f:
+            df = pd.read_csv(f, sep=";", index_col="TimeStep", parse_dates=["TimeStep"])
+        table_name = output_file.stem.lower()
+        table_name = table_name[:63]
         with engine.begin() as conn:
-            with open(output_file) as f:
-                df = pd.read_csv(
-                    f, sep=";", index_col="TimeStep", parse_dates=["TimeStep"]
-                )
-                table_name = output_file.stem.lower()
-                table_name = table_name[:63]
-                df.to_sql(table_name, conn, if_exists="replace", schema=schema_name)
+            df.to_sql(table_name, conn, if_exists="replace", schema=schema_name)
+
     print("successfully wrote to sql")
+    # kill logging from previous amiris run
+    logging.disable(logging.NOTSET)
+    reload(logging)
 
-import logging
-
-logging.disable(logging.NOTSET)
-from importlib import reload
-
-reload(logging)
-# this kills the logging config from amiris
-
+# now start the fresh assume run
 if run_assume:
     from assume import World
     from assume.scenario.loader_amiris import load_amiris_async
+
+    forecast_file = Path("scenario", scenario, "MeritOrderForecaster.csv")
+    with open(forecast_file, "r") as f:
+        forecast = pd.read_csv(
+            f, sep=";", index_col="TimeStep", parse_dates=["TimeStep"]
+        )
+
+    with open("../amiris-examples/Germany2019/timeseries/co2_price.csv", "r") as f:
+        co2 = pd.read_csv(
+            f,
+            sep=";",
+            index_col=0,
+            parse_dates=[0],
+            header=None,
+            date_format="%Y-%m-%d_%H:%M:%S",
+        )
+
+    # forecast["ElectricityPriceForecastInEURperMWH"].plot()
+    # co2.plot()
 
     print(f"running {scenario}")
     start = time.time()
 
     world = World(database_uri=db_uri)
+    # world.forecast_price = forecast["ElectricityPriceForecastInEURperMWH"]
     world.loop.run_until_complete(
         load_amiris_async(
             world,
